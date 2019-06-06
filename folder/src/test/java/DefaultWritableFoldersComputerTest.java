8841f2bd091423809ce89c139442c6b1436752cd
nicu <nicolae.marasoiu@gmail.com>
2019-06-06 18:38:00 +0300

    lists not set

--- folder/src/test/java/DefaultWritableFoldersComputerTest.java
+++ folder/src/test/java/DefaultWritableFoldersComputerTest.java
@@ -12,15 +12,14 @@ import static org.hamcrest.core.IsEqual.equalTo;
 
 public class DefaultWritableFoldersComputerTest {
 
-    private static final List<String> ROOT = Collections.singletonList("/");
-    private static final WritableFoldersComputer DEFAULT_WRITABLE_FOLDERS_COMPUTER = new DefaultWritableFoldersComputer();
+    private final WritableFoldersComputer writableFoldersComputer = new DefaultWritableFoldersComputer();
 
     @Test
     void simpleTests() {
         List<String> writableFolders = asList("/a", "/b", "/b/c/d", "/b/f/e");
         List<String> readableFolders = concat(writableFolders, asList("/", "/b/c"));
         assertThat(
-                DEFAULT_WRITABLE_FOLDERS_COMPUTER
+                writableFoldersComputer
                         .accessibleAndWritableFolders(readableFolders, writableFolders).toPaths("/")
                         .collect(Collectors.toSet()),
                 equalTo(new HashSet<>(asList("/a", "/b/c/d")))
