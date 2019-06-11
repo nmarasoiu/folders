cbbdd812334136d25fefabcaea6d35f2a6eac72f
nicolae.marasoiu <nicolae.marasoiu@tradeshift.com>
2019-06-11 12:54:53 +0300

    streaming thru

--- folder/src/main/java/DefaultWritableFoldersComputer.java
+++ folder/src/main/java/DefaultWritableFoldersComputer.java
@@ -1,17 +1,19 @@
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static com.google.common.collect.Sets.newHashSet;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 public class DefaultWritableFoldersComputer implements WritableFoldersComputer {
     private static final Pattern slashSplitter = Pattern.compile("/");
 
     @Override
     public Tree accessibleAndWritableFolders(List<String> readableFolders, List<String> writableFolders) {
-        return Tree.from(reachableAndWritableFolders(newHashSet(clean(readableFolders)), clean(writableFolders)));
+        Set<String> readableDirs = clean(readableFolders).collect(toSet());
+        List<String> writableDirs = clean(writableFolders).collect(toList());
+        return Tree.from(reachableAndWritableFolders(readableDirs, writableDirs));
     }
 
     private Stream<String> reachableAndWritableFolders(Set<String> readableFolders, List<String> writableFolders) {
@@ -24,20 +26,15 @@ public class DefaultWritableFoldersComputer implements WritableFoldersComputer {
         StringBuilder ancestorBuilder = new StringBuilder(dir.length());
         return slashSplitter
                 .splitAsStream(dir.substring(1))
-                .map(name -> {
-                    ancestorBuilder.append("/").append(name);
-                    return ancestorBuilder.toString();
-                });
+                .map(name -> ancestorBuilder.append("/").append(name).toString());
     }
 
-    private List<String> clean(List<String> readableFolders) {
+    private Stream<String> clean(List<String> readableFolders) {
         return readableFolders
                 .stream()
                 .map(String::trim)
                 .map(str -> str.replaceAll("[/]+", "/"))
-                .map(str -> str.endsWith("/") ? str.substring(0, str.length() - 1) : str)
-//                .distinct()
-                .collect(Collectors.toList());//toSet? but maybe nicer to keep the order
+                .map(str -> str.endsWith("/") ? str.substring(0, str.length() - 1) : str);
     }
 
 }
