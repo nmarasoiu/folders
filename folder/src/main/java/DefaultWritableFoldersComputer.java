6367bc29f63d968164096933cd42770b53ef85a4
nicolae.marasoiu <nicolae.marasoiu@tradeshift.com>
2019-06-11 12:51:37 +0300

    streaming thru

--- folder/src/main/java/DefaultWritableFoldersComputer.java
+++ folder/src/main/java/DefaultWritableFoldersComputer.java
@@ -1,12 +1,13 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.google.common.collect.Sets.newHashSet;
 
 public class DefaultWritableFoldersComputer implements WritableFoldersComputer {
+    private static final Pattern slashSplitter = Pattern.compile("/");
 
     @Override
     public Tree accessibleAndWritableFolders(List<String> readableFolders, List<String> writableFolders) {
@@ -16,18 +17,17 @@ public class DefaultWritableFoldersComputer implements WritableFoldersComputer {
     private Stream<String> reachableAndWritableFolders(Set<String> readableFolders, List<String> writableFolders) {
         return writableFolders
                 .stream()
-//                .distinct()
-                .filter(writableFolder -> readableFolders.containsAll(ancestors(writableFolder)));
+                .filter(writableFolder -> ancestors(writableFolder).allMatch(readableFolders::contains));
     }
 
-    private List<String> ancestors(String dir) {
-        List<String> ancestorSet = new ArrayList<>();
+    private Stream<String> ancestors(String dir) {
         StringBuilder ancestorBuilder = new StringBuilder(dir.length());
-        for (String name : dir.substring(1).split("/")) {
-            ancestorBuilder.append("/").append(name);
-            ancestorSet.add(ancestorBuilder.toString());
-        }
-        return ancestorSet;
+        return slashSplitter
+                .splitAsStream(dir.substring(1))
+                .map(name -> {
+                    ancestorBuilder.append("/").append(name);
+                    return ancestorBuilder.toString();
+                });
     }
 
     private List<String> clean(List<String> readableFolders) {
@@ -36,7 +36,8 @@ public class DefaultWritableFoldersComputer implements WritableFoldersComputer {
                 .map(String::trim)
                 .map(str -> str.replaceAll("[/]+", "/"))
                 .map(str -> str.endsWith("/") ? str.substring(0, str.length() - 1) : str)
-                .collect(Collectors.toList());
+//                .distinct()
+                .collect(Collectors.toList());//toSet? but maybe nicer to keep the order
     }
 
 }
